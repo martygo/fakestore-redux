bb9e7bb296c0a7420ecc0950e063100bcf267d9a
Martins Gouveia <gouveiamartins6@gmail.com>
2022-11-05 11:42:41 +0100

    🚧

--- src/redux/constants/CartActionTypes.js
+++ src/redux/constants/CartActionTypes.js
@@ -2,3 +2,6 @@ export const CartActionTypes = {
   ADD_CART: "ADD_CART",
   REMOVE_CART: "REMOVE_CART"
 }
+
+// database scheme: id, product, quantity
+// actions: add, remove on product on cart, clear all products on cart, calculate total price
